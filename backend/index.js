7da2f07ab06d2a20af9ee56aca7d9d7f933e6ae1
Mohit2005123 <mohitmongia2005@gmail.com>
2024-06-07 22:52:14 +0530

    Second commit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const port=3000;
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const dbUrl= process.env.ATLAS_URL; 
+const dbUrl= process.env.ATLAS_URL;
+const PORT= process.env.PORT || 3000; 
 app.use(cors());
 main().then(()=>{
     console.log('DB is connected');
@@ -12,7 +13,7 @@ main().then(()=>{
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect('mongodb+srv://mohitmongia04:akUHX8jZUBj0RDKE@cluster0.aeufap8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 }
 app.get('/', (req, res)=>{
     res.send('Hello world');
@@ -21,6 +22,6 @@ app.use(express.json());
 app.use('/api', require('./Routes/CreateUser.js'));
 app.use('/api', require('./Routes/DisplayData'));
 app.use('/api', require('./Routes/OrderData.js'));
-app.listen(port, ()=>{
+app.listen(PORT, ()=>{
     console.log('App is listening on port 3000');
 })
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate vendor chunk
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 kB
+    outDir: 'dist' // Ensure output directory is set to 'dist'
+  }
+});
